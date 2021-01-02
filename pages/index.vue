e88268f0ea42bda982480035ac8f3ba2c0862f92
geck4bee <geck4bee@gmail.com>
2021-01-02 03:16:06 +0000

    Discord対応

--- pages/index.vue
+++ pages/index.vue
@@ -269,7 +269,7 @@ export default {
                 const dateLocale = new Date(postObj.date).toLocaleDateString().replace(new RegExp(/(\/)/gi), '-')
                 content = content + '●' + dateLocale + '\n'
                 postObj.posts.map(post => {
-                    content = content + '・' + post.title + '\n' + post.URL + '\n'
+                    content = content + '・' + post.title + '\n' + '<' + post.URL + '>' + '\n'
                 })
                 content = content + '\n'
             })
